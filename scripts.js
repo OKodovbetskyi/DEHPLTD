ce17b75cc916669ccbbdb559a30ee968081d8bff
K2054852 <k2054852@kingston.ac.uk>
2023-07-10 19:04:52 +0100

    form working

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const messageinfo={};
 $(window).on('scroll', function() {
     var windowHeight = $(window).height();
     var scrollHeight = $(window).scrollTop();
@@ -8,4 +9,28 @@ $(window).on('scroll', function() {
       aboutSection.find('.fade-in').addClass('show');
     }
   });
-  
+const sendEmail = (data) => {
+    axios
+      .post("http://softwarehub.uk:3001/contact-me",data)
+      .then((response) => {
+        $(".feedback").addClass("show");
+        $("#name").val("");
+        $("#contactdetail").val("");
+        $("#message").val("");
+
+      })
+      .catch((error) => false);
+  };
+$('form').on('submit',async (e)=>{
+  e.preventDefault();
+  var name = $("#name").val();
+  var contactdetail = $("#contactdetail").val();
+  var message = $("#message").val();
+  var isChecked = $("#consent").prop("checked");
+  messageinfo.name = name;
+  messageinfo.contact = contactdetail;
+  messageinfo.message= message;
+  messageinfo.consent = "I am happy to be contacted regarding my request"
+  sendEmail(messageinfo);
+})
+
