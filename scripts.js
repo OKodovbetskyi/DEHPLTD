bd9cacd3209e0c6658dac280bcde8e663ca6e78b
OKodovbetskyi <oleksandr.kodovbetskyi@gmail.com>
2023-12-17 23:36:33 +0000

    Changed sizes on mobile screen

--- scripts.js
+++ scripts.js
@@ -1,70 +1,69 @@
-const messageinfo={};
-$(window).on('scroll', function() {
-    var windowHeight = $(window).height();
-    var scrollHeight = $(window).scrollTop();
-    var aboutSection = $('.about');
-    var aboutSectionOffset = aboutSection.offset().top;
-  
-    if (scrollHeight + windowHeight > aboutSectionOffset) {
-      aboutSection.find('.fade-in').addClass('show');
-    }
-  });
-const sendEmail = (data) => {
-    axios
-      .post("http://softwarehub.uk/api/email/contact-me",data)
-      .then((response) => {
-        $(".feedback").addClass("show");
-        $("#name").val("");
-        $("#contactdetail").val("");
-        $("#message").val("");
+const messageinfo = {};
+$(window).on("scroll", function () {
+  var windowHeight = $(window).height();
+  var scrollHeight = $(window).scrollTop();
+  var aboutSection = $(".about");
+  var aboutSectionOffset = aboutSection.offset().top;
 
-      })
-      .catch((error) => false);
-  };
-$('form').on('submit',async (e)=>{
+  if (scrollHeight + windowHeight > aboutSectionOffset) {
+    aboutSection.find(".fade-in").addClass("show");
+  }
+});
+const sendEmail = (data) => {
+  axios
+    .post("https://softwarehub.uk/emailservice/contact-me", data)
+    .then((response) => {
+      $(".feedback").addClass("show");
+      $("#name").val("");
+      $("#contactdetail").val("");
+      $("#message").val("");
+    })
+    .catch((error) => false);
+};
+$("form").on("submit", async (e) => {
   e.preventDefault();
   var name = $("#name").val();
   var contactdetail = $("#contactdetail").val();
   var message = $("#message").val();
   var isChecked = $("#consent").prop("checked");
   messageinfo.name = name;
-  messageinfo.contact = contactdetail;
-  messageinfo.message= message;
-  messageinfo.consent = "I am happy to be contacted regarding my request"
+  messageinfo.email = contactdetail;
+  messageinfo.text =
+    "Someone contacted you from DEHPUK said: " + `"${message}"`;
+  messageinfo.receiverEmail = "dmytro@dehp.uk";
   sendEmail(messageinfo);
-})
+});
 
 $(() => {
   const logo = $("#main-logo");
   const targetSection = $("#company-name");
-  if ($(window).width() <= 767) { 
-  $(window).scroll(() => {
-    const scrollPosition = $(window).scrollTop();
-    const targetOffset = targetSection.offset().top;
-    if (scrollPosition >= targetOffset) {
-      logo.fadeOut(300);
-    } else {
-      logo.fadeIn(300);
-    }
-  });
-}
+  if ($(window).width() <= 767) {
+    $(window).scroll(() => {
+      const scrollPosition = $(window).scrollTop();
+      const targetOffset = targetSection.offset().top;
+      if (scrollPosition >= targetOffset) {
+        logo.fadeOut(300);
+      } else {
+        logo.fadeIn(300);
+      }
+    });
+  }
 });
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
-cards.forEach(card => {
-  card.addEventListener('mouseenter', () => {
-    const video = card.querySelector('video');
+cards.forEach((card) => {
+  card.addEventListener("mouseenter", () => {
+    const video = card.querySelector("video");
     if (video) {
       video.play();
     }
   });
 
-  card.addEventListener('mouseleave', () => {
-    const video = card.querySelector('video');
+  card.addEventListener("mouseleave", () => {
+    const video = card.querySelector("video");
     if (video) {
       video.pause();
       video.currentTime = 0;
     }
   });
 });
-
